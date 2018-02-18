79c0090cb010e60909a3f3dcdc72215965edb926
Mako <mnfeconicu41@gmail.com>
2018-02-18 20:17:32 +0900

    refactoring n6.c

--- assignment/n6.c
+++ assignment/n6.c
@@ -8,6 +8,7 @@
 //    4/(8n+1) - 2/(8n+4) - 1/(8n+5) - 1/(8n+6)
 //  = (120 n^2 + 151 n + 47) / ((2 n + 1) (4 n + 3) (8 n + 1) (8 n + 5))
 //  = (120 n^2 + 151 n + 47) / (512 n^4 + 1024 n^3 + 712 n^2 + 194 n + 15)
+//  = (n (120 n + 151) + 47) / (n (n (n (512 n + 1024) + 712) + 194) + 15)
 
 #include <stdio.h>
 #include "integer.h"
@@ -17,7 +18,7 @@ int main(void) {
     int extraDigits = 10;
     int calcDigits = digits + extraDigits;
     Number tmp, _;
-    Number n1, n2, n3, n4, num16n;
+    Number n, num16n;
     Number term, quot;
     Number numer, denom; // numer/denom
     Number one, sixteen, onetwenty, onefiftyone, fortyseven;
@@ -35,10 +36,7 @@ int main(void) {
     setInt(&oneninetyfour, 194);
     setInt(&fifteen, 15);
 
-    clearByZero(&n1);
-    clearByZero(&n2);
-    clearByZero(&n3);
-    clearByZero(&n4);
+    clearByZero(&n);
     clearByZero(&result);
 
     mulBy10E(calcDigits, &one, &prec); // prec = 1e+digits
@@ -47,31 +45,27 @@ int main(void) {
         clearByZero(&numer);
         clearByZero(&denom);
 
-        multiple(&n1, &n1, &n2); // n^2 = n^1 * n^1
-        multiple(&n2, &n1, &n3); // n^3 = n^2 * n^1
-        multiple(&n2, &n2, &n4); // n^4 = n^2 * n^2
-
-        // numer = 120 n^2 + 151 n^1 + 47
-        multiple(&onetwenty, &n2, &term);
-        add(&numer, &term, &tmp); copyNumber(&tmp, &numer);
-        multiple(&onefiftyone, &n1, &term);
-        add(&numer, &term, &tmp); copyNumber(&tmp, &numer);
+        // numer = (n (120 n + 151) + 47)
+        copyNumber(&onetwenty, &numer);
+        multiple(&n, &numer, &tmp); copyNumber(&tmp, &numer);
+        add(&numer, &onefiftyone, &tmp); copyNumber(&tmp, &numer);
+        multiple(&n, &numer, &tmp); copyNumber(&tmp, &numer);
         add(&numer, &fortyseven, &tmp); copyNumber(&tmp, &numer);
 
-        // denom = 512 n^4 + 1024 n^3 + 712 n^2 + 194 n + 15
-        multiple(&fivetwelve, &n4, &term);
-        add(&denom, &term, &tmp); copyNumber(&tmp, &denom);
-        multiple(&tentwentyfour, &n3, &term);
-        add(&denom, &term, &tmp); copyNumber(&tmp, &denom);
-        multiple(&seventwelve, &n2, &term);
-        add(&denom, &term, &tmp); copyNumber(&tmp, &denom);
-        multiple(&oneninetyfour, &n1, &term);
-        add(&denom, &term, &tmp); copyNumber(&tmp, &denom);
+        // denom = (n (n (n (512 n + 1024) + 712) + 194) + 15)
+        copyNumber(&fivetwelve, &denom);
+        multiple(&n, &denom, &tmp); copyNumber(&tmp, &denom);
+        add(&denom, &tentwentyfour, &tmp); copyNumber(&tmp, &denom);
+        multiple(&n, &denom, &tmp); copyNumber(&tmp, &denom);
+        add(&denom, &seventwelve, &tmp); copyNumber(&tmp, &denom);
+        multiple(&n, &denom, &tmp); copyNumber(&tmp, &denom);
+        add(&denom, &oneninetyfour, &tmp); copyNumber(&tmp, &denom);
+        multiple(&n, &denom, &tmp); copyNumber(&tmp, &denom);
         add(&denom, &fifteen, &tmp); copyNumber(&tmp, &denom);
 
         multiple(&numer, &prec, &tmp); copyNumber(&tmp, &numer); // numer *= prec
 
-        power(&sixteen, &n1, &num16n);
+        power(&sixteen, &n, &num16n);
         multiple(&denom, &num16n, &tmp); copyNumber(&tmp, &denom); // denom *= 16^n
 
         if (compNumber(&numer, &denom) < 0) break;
@@ -79,7 +73,7 @@ int main(void) {
         add(&result, &quot, &tmp); copyNumber(&tmp, &result);
 
         // next
-        increment(&n1, &tmp); copyNumber(&tmp, &n1);
+        increment(&n, &tmp); copyNumber(&tmp, &n);
     }
 
     divBy10E(extraDigits, &result, &tmp); copyNumber(&tmp, &result);
