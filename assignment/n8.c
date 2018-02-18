33b6ade10fd0b1eb4916f295b6c07ded62bb1107
Mako <mnfeconicu41@gmail.com>
2018-02-18 19:23:56 +0900

    refactoring assignment/n8.c

--- assignment/n8.c
+++ assignment/n8.c
@@ -35,49 +35,22 @@ int main(void) {
         decrement(&base, &numer);
         divmodByInt(&numer, &nine_, &R_n, &rem);
 
-        // --- Factoring ---
-        // TODO: functionarize
+        printf("R_%d = ", n); dispNumberZeroSuppress(&R_n); putchar('\n');
 
-        sqrtNumber(&R_n, &max); // max = sqrt(R_n)
-        copyNumber(&R_n, &num); // num = R_n
-
-        printf("R_%d = ", n); dispNumberZeroSuppress(&num); putchar('\n');
-
-        setInt(&divisor, 3);
-        isPrimeNumber = TRUE;
-        while (1) {
-            if (compNumber(&divisor, &max) > 0) break;
-
-            // skip if divisor != 5 && divisor % 5 == 0
-            if (compNumber(&divisor, &five) != 0 && divisor.n[0] % 10 == 5) {
-                add(&divisor, &two, &tmp); copyNumber(&tmp, &divisor); // divisor += 2
-                continue;
-            }
-
-            divmod(&num, &divisor, &quot, &remain);
-            if (compNumber(&remain, &zero) == 0) {
-                factors[factor_i++] = divisor;
-                copyNumber(&quot, &num);
-                isPrimeNumber = FALSE;
-                continue;
-            }
-
-            add(&divisor, &two, &tmp); copyNumber(&tmp, &divisor); // divisor += 2
-        }
-        if (!isPrimeNumber && compNumber(&num, &one) != 0) {
-            factors[factor_i++] = num;
-        }
-
-        if (isPrimeNumber) {
+        // Factorization
+        int factorCount = factorizeNumber(&R_n, factors, 100);
+        if (factorCount == 0) {
             printf("R_%d is prime number.\n", n);
-        } else {
+        } else if (factorCount > 0) {
             printf("R_%d is not a prime number.\n", n);
             puts("Factors:");
-            for (i = 0; i < factor_i; i++) {
+            printf("factorCount = %d\n", factorCount);
+            for (i = 0; i < factorCount; i++) {
                 dispNumberZeroSuppress(&factors[i]); putchar('\n');
             }
+        } else {
+            fprintf(stderr, "R_%d: factorCount is overflow (max: %d).\n", n, 100);
         }
-
         printf("\n");
 
         // next
