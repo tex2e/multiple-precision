2c2480235761cb72089f1dc3b2119879462f71b6
Mako <mnfeconicu41@gmail.com>
2018-02-22 18:41:33 +0900

    create ruby-bigdecimal/impl-fast-divmod.rb

---
+++ ruby-bigdecimal/impl-fast-divmod.rb
@@ -0,0 +1,59 @@
+
+a = 400_000_000_000
+b = 500_999
+
+def fast_divmod(_a, _b, radix:)
+  a, b = _a, _b
+
+  radix_len = Math.log10(radix).to_i
+  as = a.to_s.scan(/.{1,#{radix_len}}/).map(&:to_i).reverse
+  bs = b.to_s.scan(/.{1,#{radix_len}}/).map(&:to_i).reverse
+  qs = [] # ans
+  s = as.length - 1
+  t = bs.length - 1
+  k = radix / (1 + bs[t])
+
+  a *= k
+  b *= k
+
+  while 1
+    as = a.to_s.scan(/.{1,#{radix_len}}/).map(&:to_i).reverse
+    bs = b.to_s.scan(/.{1,#{radix_len}}/).map(&:to_i).reverse
+    s = as.length - 1
+    t = bs.length - 1
+    p [a, b, as, bs, s, t]
+    u = 0
+    qu = 0
+    if as[s] < bs[t]
+      u = s - t - 1
+      qu = (as[s] * radix + as[s-1]) / bs[t]
+      while b * qu * radix**u < a
+        qu -= 1
+      end
+    else
+      if a < b * radix**(s - t)
+        u = s - t - 1
+        qu = radix - 1
+        while b * qu * radix**u < a
+          qu -= 1
+        end
+      else
+        u = s - t
+        qu = 1
+      end
+    end
+    qs[u] = qu
+    a -= b * qu * radix**u
+    while a < 0
+      qs[u] -= 1
+      a += b * radix**u
+    end
+    if u <= 0
+      return [qs.reverse.join.to_i, a / k]
+    end
+  end
+end
+
+res = fast_divmod(a, b, radix: 1000)
+puts "result   = #{res}"
+puts "expected = #{a.divmod(b)}"
