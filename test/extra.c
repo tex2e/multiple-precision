d62a5663c4aaa92c300184e1fdf99f613f723863
Mako <mnfeconicu41@gmail.com>
2018-02-26 20:54:09 +0900

    test: update tset_sqrtNumber in extra.c

--- test/extra.c
+++ test/extra.c
@@ -39,15 +39,12 @@ void test_sqrtNumber() {
 
 void test_inverseNumber() {
     printf("* test_inverseNumber\n");
-    int expo_expected = 20;
-    int expo_result;
     char* expected = "1785714285714285714";
     char result[KETA * RADIX_LEN + 2];
     Number tmp;
     Number a, b;
     setInt(&a, 56);
-    expo_result = inverseNumber(&a, 40, &b);
-    divBy10E(20, &b, &tmp); copyNumber(&tmp, &b);
+    inverseNumber(&a, 20, &b);
     getStr(&b, result);
     // printf("expected = %s\n", expected);
     // printf("result   = %s\n", result);
